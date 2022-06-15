8fa3d34f0926a40ec8dcf580dd3e233d2899f9ff
Leon Seemann <le.see@web.de>
2022-06-15 12:17:11 +0200

    Added progress bar

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 import urllib.request
 from pathlib import Path
 
-from tqdm import tqdm
-
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -15,5 +13,7 @@ howMuch = int(input("How much: "))
 
 Path(nameOfFiles).mkdir(parents=True, exist_ok=True)
 
-for x in tqdm(range(1, howMuch + 1)):
+for x in range(1, howMuch + 1):
     urllib.request.urlretrieve(url, nameOfFiles + "/" + nameOfFiles + "-" + str(x) + ".jpg")
+    cls()
+    print(str(x / howMuch * 100))
