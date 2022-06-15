ab627182dc2f09f2d01f71bbaba68ed4c7afb2ad
Leon Seemann <le.see@web.de>
2022-06-15 08:40:52 +0200

    Added gitignore Added new Clear method

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import urllib.request
 import os
 
-clear = lambda: os.system('cls')
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 url = str(input("Url: "))
+nameOfFiles = str(input("Name of files: "))
+howMuch = int(input("How much: "))
 
-howMuch = int(input("Wie viele bilder? "))
+cls()
 
 for x in range(1, howMuch + 1):
-    urllib.request.urlretrieve(url, "background-" + str(x) + ".jpg")
-    clear()
+    urllib.request.urlretrieve(url, nameOfFiles + "-" + str(x) + ".jpg")
+    cls()
     print(str(x / howMuch * 100))
